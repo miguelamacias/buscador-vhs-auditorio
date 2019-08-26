b03e41ed66f4295a72f635019d9eb62deebe28f0
Macías <miguel.a.macias.13@gmail.com>
2019-08-26 16:07:23 +0200

    La funcionalidad básica funciona.
    Hace falta ponerle la base de datos real y ponerlo "bonito"...

--- BuscadorVHSAuditorio/src/application/Main.java
+++ BuscadorVHSAuditorio/src/application/Main.java
@@ -1,26 +1,127 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.awt.EventQueue;
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Insets;
+import javax.swing.JScrollPane;
+
+public class Main {
+
+	private JFrame frame;
+	private JTextField cuadroBusquedaTexto;
+	private JTextField cuadroBuscarNumero;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		launch(args);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		SingletonDB db = SingletonDB.getInstancia();
+		frame = new JFrame();
+		frame.setBounds(100, 100, 935, 674);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		cuadroBusquedaTexto = new JTextField();
+		cuadroBusquedaTexto.setBounds(80, 97, 280, 30);
+		frame.getContentPane().add(cuadroBusquedaTexto);
+		cuadroBusquedaTexto.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 241, 893, 373);
+		frame.getContentPane().add(scrollPane);
+		
+		JTextArea areaResultados = new JTextArea();
+		scrollPane.setViewportView(areaResultados);
+		areaResultados.setMargin(new Insets(2, 10, 10, 10));
+		areaResultados.setEditable(false);
+		areaResultados.setFont(new Font("Consolas", Font.PLAIN, 14));
+		
+		cuadroBuscarNumero = new JTextField();
+		cuadroBuscarNumero.setColumns(10);
+		cuadroBuscarNumero.setBounds(650, 97, 102, 30);
+		frame.getContentPane().add(cuadroBuscarNumero);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				areaResultados.setText(db.buscarTexto(cuadroBusquedaTexto.getText()));
+			}
+		});
+		btnBuscar.setBounds(372, 97, 97, 30);
+		frame.getContentPane().add(btnBuscar);
+		
+		JButton btnMostrar = new JButton("Mostrar");
+		btnMostrar.setBounds(764, 97, 97, 30);
+		frame.getContentPane().add(btnMostrar);
+		
+		
+		
+		JLabel lblCdigo = new JLabel("Código");
+		lblCdigo.setBounds(26, 212, 56, 16);
+		frame.getContentPane().add(lblCdigo);
+		
+		JLabel lblContenido = new JLabel("Contenido");
+		lblContenido.setBounds(112, 212, 75, 16);
+		frame.getContentPane().add(lblContenido);
+		
+		JLabel lblFecha = new JLabel("Fecha");
+		lblFecha.setBounds(764, 212, 56, 16);
+		frame.getContentPane().add(lblFecha);
+		
+		JLabel lblContenido_1 = new JLabel("Contenido");
+		lblContenido_1.setBounds(12, 104, 116, 16);
+		frame.getContentPane().add(lblContenido_1);
+		
+		JLabel lblFecha_1 = new JLabel("o Fecha");
+		lblFecha_1.setBounds(12, 118, 56, 16);
+		frame.getContentPane().add(lblFecha_1);
+		
+		JLabel lblBuscarPor = new JLabel("Buscar por");
+		lblBuscarPor.setBounds(12, 89, 75, 16);
+		frame.getContentPane().add(lblBuscarPor);
+		
+		JLabel label = new JLabel("Buscar por");
+		label.setBounds(546, 96, 75, 16);
+		frame.getContentPane().add(label);
+		
+		JLabel lblNumeorDeCinta = new JLabel("numero de cinta");
+		lblNumeorDeCinta.setBounds(546, 111, 102, 16);
+		frame.getContentPane().add(lblNumeorDeCinta);
+		
+		JLabel lblBuscadorVhsAuditorio = new JLabel("Buscador VHS Auditorio");
+		lblBuscadorVhsAuditorio.setFont(new Font("Britannic Bold", Font.PLAIN, 35));
+		lblBuscadorVhsAuditorio.setBounds(12, 13, 387, 59);
+		frame.getContentPane().add(lblBuscadorVhsAuditorio);
 	}
 }
---
+++ BuscadorVHSAuditorio/src/application/SingletonDB.java
@@ -0,0 +1,64 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SingletonDB {
+	public static SingletonDB instancia = new SingletonDB();
+	
+	
+	public SingletonDB() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String buscarTexto(String textoBuscar) {
+		StringBuilder listado = new StringBuilder();
+		
+		Connection conexion = null;
+		PreparedStatement sentencia = null;
+		ResultSet resultado = null;
+		
+		try {
+			conexion = DriverManager.getConnection("jdbc:sqlite:prueba.db");
+			sentencia = conexion.prepareStatement("SELECT codigo, contenido, fecha FROM vhs WHERE contenido LIKE ? ORDER BY codigo");
+			sentencia.setString(1, "%"+textoBuscar+"%");
+			resultado = sentencia.executeQuery();
+			
+			
+			while (resultado.next()) {
+				listado.append(String.format("%-10s%-82s%s%n", resultado.getInt(1), resultado.getString(2), resultado.getString(3)));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (sentencia != null) {
+				try {
+					sentencia.close();
+				} catch (SQLException e2) {
+					e2.printStackTrace();
+				}
+			}
+			if (conexion != null) {
+				try {
+					conexion.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}	
+		}
+		
+		return listado.toString();
+	}
+	
+	public static SingletonDB getInstancia() {
+		return instancia;
+	}
+}
